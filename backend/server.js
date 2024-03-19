bd28c7d3b058e858a578ea33805317250177283c
piyushgr <piyushgaur0001@gmail.com>
2024-03-19 22:10:12 +0530

    New Device Setup Done

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use('/api/v1/auth',authRoute);
 
 app.get('/',(req,res)=>{
     res.send({
-        message:"Welcome to Ecom",
+        message:"Welcome to Ecom.",
     })
 })
 app.listen(PORT,()=>{
