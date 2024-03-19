f890c0fe8801dea07f76b019175a23d613f5d65d
piyushgr <piyushgaur0001@gmail.com>
2024-03-19 21:39:40 +0530

    changed device

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,7 @@ app.use('/api/v1/auth',authRoute);
 
 app.get('/',(req,res)=>{
     res.send({
-        message:"hello ",
-        route:"/"
+        message:"Welcome to Ecom",
     })
 })
 app.listen(PORT,()=>{
